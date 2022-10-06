8732f36f46bee74af5ad9736a16b12516e3a8e4d
Joshua Ramos <joshua.m.ramos32@gmail.com>
2022-10-06 11:32:16 -0500

    adding JavaScripthtml

---
+++ js/external.js
@@ -0,0 +1,55 @@
+"use strict";
+
+console.log('Hello from external JavaScript');
+alert('Welcome to my website');
+let userInput = prompt('What is your favorite color?');
+alert ('Awesome! My favorite color is also ' + userInput);
+
+
+// movie rental
+let theLittleMermaid =3;
+prompt('How many days did you have The Little Mermaid?');
+let brotherBear =5 ;
+prompt ('How many days did you have Brother Bear?');
+let hercules =1 ;
+const price = 3
+let total = theLittleMermaid + brotherBear + hercules;
+let cost = total * price;
+alert (cost);
+
+// contractor pay
+let amazon = 380 * 4;
+prompt( 'How many hours did you work at Amazon this week?')
+let google = 400 * 6;
+prompt ('How many hours did you work at Google this week?')
+let facebook = 350 * 10;
+prompt (' How many hours did you work at Facebook this week?')
+let pay = amazon + google + facebook;
+alert ( 'your total pay is ' + pay);
+
+
+// class enrollment
+
+alert ("It is time for your enrollment");
+prompt( 'Are you enrolled?');
+prompt(' is the class full?');
+prompt('Is there a schedule conflict?')
+alert(' You are good to enroll!')
+
+//product offer
+prompt('Are you a premium member?')
+alert ('The product offer is valid for you!')
+prompt('have you bought more then two items?')
+alert ('Product offer is valid')
+
+
+
+
+
+
+
+
+
+
+
+
