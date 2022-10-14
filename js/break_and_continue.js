93900713f62dc4b6b503d2497a05fd33622fa576
Joshua Ramos <joshua.m.ramos32@gmail.com>
2022-10-14 15:09:26 -0500

    adding break_and_continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,13 +1,18 @@
-let oddNumber = prompt("Please enter an odd number between 1 and 50? ");
-let numberToSkip = oddNumber
-for ( let i =50; i > 0; i -=3){
-		console.log(i - numberToSkip);
-	if(i !== oddNumber){ continue;}
+let oddNumber;
+
+while (true){
+
+	oddNumber = prompt("Please enter an odd number between 1 and 50? ");
+	if(oddNumber % 2 !== 0 && oddNumber > 1 && oddNumber < 50){
 		break;
 	}
+}
+let numberToSkip = parseInt(oddNumber)
+for ( let i =1; i <=50; i+=2) {
+	if (i === numberToSkip) {
+		console.log("number to skip is " + i);
+		continue;
+	}
+	console.log("Here is an odd number " + i)
+}
 
-
-//let text = "";
-// for (let i = 0; i < 10; i++) {
-//   if (i === 3) { continue; }
-//   text += "The number is " + i + "<br>";
