bd5dcd86911541b57c8619305b869a34b266bce2
Joshua Ramos <joshua.m.ramos32@gmail.com>
2022-10-14 15:30:05 -0500

    adding while loop

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,5 @@
 let oddNumber;
-
 while (true){
-
 	oddNumber = prompt("Please enter an odd number between 1 and 50? ");
 	if(oddNumber % 2 !== 0 && oddNumber > 1 && oddNumber < 50){
 		break;
---
+++ js/while.js
@@ -0,0 +1,7 @@
+let i = 1;
+
+while (i < 65536) {
+	let number = i * 2;
+	console.log(number);
+	i*=2;
+}
