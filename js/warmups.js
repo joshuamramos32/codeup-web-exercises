be5f2a3a9240badd07045d87f2440c3ac7aa7ed6
Joshua Ramos <joshua.m.ramos32@gmail.com>
2022-10-21 09:56:34 -0500

    adding html forms

--- js/warmups.js
+++ js/warmups.js
@@ -11,4 +11,38 @@ let potatoes ={
 function getPrice(obj){
 	return obj.price;
 }
-console.log(getPrice(potatoes))
+console.log(getPrice(potatoes));
+
+// Write a function that when passed an array will return the longest string from the array.
+// 	Ex. const arr = [“Green”, “Blue”, “Periwinkle”];
+// getLongestString(arr) // Returns “Periwinkle”
+
+//my code below= wrong
+// const arr =["Green","Blue","Periwinkle"];
+//
+//
+// function getLongestString() {
+// 	return arr.length;
+// }
+//
+//
+
+// walk through code
+// console.log(getLongestString(arr));
+//
+// const colors =["green", "blue","periwinkle"];
+// function getLongestString(){
+// 	let longestStr = "";
+// 	for (let str of arr){
+// 		if (str.length > longestStr.length){
+// 			longestStr = str;
+//
+// 		}
+// 	}
+// 	return longestStr
+// }
+// console.log(getLongestString(colors))
+
+
+
+
