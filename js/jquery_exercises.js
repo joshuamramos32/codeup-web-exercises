5449f71aa92aaf1672577f4ef835a2043b7906af
Joshua Ramos <joshua.m.ramos32@gmail.com>
2022-11-17 14:50:49 -0600

    adding jquery exercies

---
+++ js/jquery_exercises.js
@@ -0,0 +1,36 @@
+
+$(function(){
+	// alert($("#my-paragraph").html());
+	// alert($("#my-div").html());
+	// $(".codeup").css("border", "1px solid red");
+	// $("li").css("font-size"," 20px")
+	// $("h1").css("background-color", "yellow")
+	// $("li").css("background-color", "lightgreen")
+	// $("p").css("background-color", "cyan")
+	$("h1, p, li").css("background-color", "yellow")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
