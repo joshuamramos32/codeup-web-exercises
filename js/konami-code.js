c561a1ea576d73ada8eed0c5548f1e22d5442ea6
Joshua Ramos <joshua.m.ramos32@gmail.com>
2022-11-21 08:55:14 -0600

    adding konami updates

--- js/konami-code.js
+++ js/konami-code.js
@@ -4,9 +4,7 @@ $(document).keyup(function(event){
 	console.log(event.keyCode);
 });
 
-// $(".header").css({
-//  "color": "yellow"
-// })
+
 function contraCheat(){
 let cheatKeys = { 37: "left", 38: "up", 39: "right", 40: "down", 65: "a", 66: "b", 13: "enter"};
 let cheatCode = ["up","up","down","down","left","right","left","right","b","a","enter"]
@@ -58,20 +56,3 @@ superMarioGif();
 
 
 
-// $(document).keydown(function(e){
-//  for (let i = 0; i < cheatCode.length; i++){
-// 	if (cheatPosition === cheatCode.length){
-// 		alert("!");
-// 	}else{
-// 		cheatPosition = ++cheatPosition;
-// 	}
-// }
-// })
-//
-
-
-
-// let buttonsPressed = $("#up, #down, #left, #right, #buttonB, #buttonA, #enter").keyup(function(){
-// 	if(cheatkeys.length === buttonsPressed );
-// 		alert("You did it");
-// })
