a064ead63fbfe09ffbb766f9f2fad7fa12f79dcb
Joshua Ramos <joshua.m.ramos32@gmail.com>
2022-12-08 11:53:54 -0600

    Adding changes

--- js/promises.js
+++ js/promises.js
@@ -5,9 +5,10 @@
 		return fetch('https://api.github.com/repos/joshuamramos32/codeup-web-exercises/commits?per_page=1')
 			.then(res => res.json())
 			.then(res => {
-				document.getElementById('message').innerHTML = res[0].commit.author.date
+				document.getElementById('message').innerHTML = res[0].commit.author.date;
+				document.getElementById('message2').innerHTML = res[0].commit.message;
 			},)
-		console.log(lastCommit());
+
 	}
 
 	console.log(lastCommit());
